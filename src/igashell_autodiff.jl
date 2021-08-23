f274f28b2d2fdabfe6d67bb37682d79408fbb6ca
Elias <elias.isak.borjesson@gmail.com>
2021-08-23 13:06:22 +0200

    make autodiff stress_recovory not allocate

--- src/igashell_autodiff.jl
+++ src/igashell_autodiff.jl
@@ -1,7 +1,7 @@
 
-function _calculate_X₀(msip::Interpolation, coords::Vector{Vec{dim_s,T2}}, ξ::Vec{dim_s,T})where {dim_s,T,T2}
+function _calculate_X₀(msip::Interpolation, coords::Vector{Vec{dim_s,Float64}}, ξ::Vec{dim_s,T})::Vec{dim_s,T} where {dim_s,T}
     dim_p = dim_s-1
-    ξ2 = Vec((ξ[1:dim_p]...,))
+    ξ2 = Vec{dim_p,T}(i->ξ[i])
 
     x = zero(Vec{dim_s,T})
     for i in 1:getnbasefunctions(msip)
@@ -15,9 +15,9 @@ function _calculate_E(msip::Interpolation, coords::Vector{Vec{dim_s,T2}}, ξ::Ve
     return gradient((ξ) -> _calculate_X₀(msip, coords, ξ), ξ)[:,α]
 end
 
-function _calculate_D(msip::Interpolation, coords::Vector{Vec{dim_s,T2}}, h::T2, ξ::Vec{dim_s,T}) where {dim_s,T,T2}
+function _calculate_D(msip::Interpolation, coords::Vector{Vec{2,T2}}, h::T2, ξ::Vec{2,T}) where {T,T2}
     
-    E = [gradient((ξ) -> _calculate_X₀(msip, coords, ξ), ξ)[:,d] for d in 1:(dim_s-1)]
+    E = gradient((ξ) -> _calculate_X₀(msip, coords, ξ), ξ)
 
     D = cross(E...)
     D /= norm(D)
@@ -25,26 +25,38 @@ function _calculate_D(msip::Interpolation, coords::Vector{Vec{dim_s,T2}}, h::T2,
     return D
 end
 
-function _calculate_X(msip::Interpolation, coords::Vector{Vec{dim_s,T2}}, h::T2, ξ::Vec{dim_s,T})where {dim_s,T,T2}
+function _calculate_D(msip::Interpolation, coords::Vector{Vec{3,T2}}, h::T2, ξ::Vec{3,T}) where {T,T2}
+    
+    _E = gradient((ξ) -> _calculate_X₀(msip, coords, ξ), ξ)
+    E1 = _E[:,1]
+    E2 = _E[:,2]
+
+    D = Tensors.cross(E1,E2)
+    D /= norm(D)
+    D *= 0.5h
+    return D
+end
+
+function _calculate_X(msip::Interpolation, coords::Vector{Vec{dim_s,T2}}, h::T2, ξ::Vec{dim_s,T})::Vec{dim_s,T} where {dim_s,T,T2}
     X₀ = _calculate_X₀(msip, coords, ξ)
     D = _calculate_D(msip, coords, h, ξ)
     ζ = ξ[dim_s]
     return X₀ + ζ*D
 end
 
-function _calculate_x(ip::Interpolation, coords::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}) where {dim_s,T,T2}
+function _calculate_x(ip::Interpolation, coords::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2})::Vec{dim_s,T2} where {dim_s,T,T2}
     X = _calculate_X(ip, coords, h, ξ)
     u = _calculate_u(ip, ue, ξ)
     return X + u
 end
 
-function _calculate_x(ip::Tuple{Interpolation, Vector{Interpolation}, IGA.BezierExtractionOperator}, coords::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}) where {dim_s,T,T2}
+function _calculate_x(ip::Tuple{Interpolation, Vector{Interpolation}, IGA.BezierExtractionOperator}, coords::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2})::Vec{dim_s,T2} where {dim_s,T,T2}
     X = _calculate_X(ip[1], coords, h, ξ)
     u = _calculate_u(ip, ue, ξ)
     return X + u
 end
 
-function _calculate_u(ip::Tuple{Interpolation, Vector{Interpolation}, IGA.BezierExtractionOperator}, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}) where {dim_s,T,T2}
+function _calculate_u(ip::Tuple{Interpolation, Vector{Interpolation}, IGA.BezierExtractionOperator}, ue::AbstractVector{T}, ξ::Vec{dim_s,T2})::Vec{dim_s,T2} where {dim_s,T,T2}
     
     msip = ip[1]
     opip = ip[2]
@@ -52,8 +64,8 @@ function _calculate_u(ip::Tuple{Interpolation, Vector{Interpolation}, IGA.Bezier
 
     dim_p = dim_s-1
 
-    ξ2 = Vec((ξ[1:dim_p]...,))
-    ζ = Vec((ξ[dim_s]))
+    ξ2 = Vec{dim_p,T2}(i->ξ[i])
+    ζ = Vec{1,T2}((ξ[3],))
 
     N = T2[]
     B = [Ferrite.value(msip, i, ξ2) for i in 1:getnbasefunctions(msip)]
@@ -79,10 +91,10 @@ function _calculate_u(ip::Tuple{Interpolation, Vector{Interpolation}, IGA.Bezier
     return u
 end
 
-function _calculate_u(ip::Interpolation, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}) where {dim_s,T,T2}
+function _calculate_u(ip::Interpolation, ue::AbstractVector{T}, ξ::Vec{dim_s,T2})::Vec{dim_s,T2} where {dim_s,T,T2}
     
     u = zero(Vec{dim_s,T2})
-    ξ2 = Vec(ntuple(i->ξ[i], dim_s-1))
+    ξ2 = Vec{dim_s-1,T2}(i->ξ[i])
 
     count = 1
     for i in 1:getnbasefunctions(ip)
@@ -111,37 +123,39 @@ function _calculate_g(ip, X::Vector{Vec{dim_s,T2}}, h::T2, ue::AbstractVector{T2
 end
 
 
-function calculate_a(ip, X::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}, α::Int) where {dim_s, T,T2}
+function calculate_a(ip, X::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}, α::Int)::T2 where {dim_s, T,T2}
     g = _calculate_g(ip, X, h, ue, ξ, α)
     a = sqrt(g⋅g)
     return a
 end
 
-function calculate_stress_recovory_variables(ip, X::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}) where {dim_s, T,T2}
+function calculate_stress_recovory_variables!(a, da, ip, X::Vector{Vec{dim_s,T}}, h::T, ue::AbstractVector{T}, ξ::Vec{dim_s,T2}) where {dim_s, T,T2}
 
     dim_p = dim_s-1
 
-    da = [gradient((ξ)->calculate_a(ip, X, h, ue, ξ, i), ξ) for i in 1:dim_p]
-    #da = Tensor{2,dim_p,T}((da[1][1], da[1][2], da[2][1], da[2][2]))
+    for i in 1:dim_p
+        da[i] = gradient((ξ)->calculate_a(ip, X, h, ue, ξ, i), ξ)   
+    end
 
-    a = [calculate_a(ip, X, h, ue, ξ, i) for i in 1:dim_p]
-    #a = Tensor{1,dim_p,T}(Tuple(a))
+    for i in 1:dim_p
+        a[i] = calculate_a(ip, X, h, ue, ξ, i)
+    end
+    #=
+    for i in 1:dim_p
+        g[i] = _calculate_g(ip, X, h, ue, ξ, d)
+        #∇g₃ = gradient((ξ)->_calculate_g(ip, X, h, ue, ξ, dim_s), ξ)[:, i]
+        E[i] = _calculate_E(ip, X, ξ, i)
+        Dₐ[i] = gradient((ξ)->_calculate_D(ip, X, h, ξ), ξ)[:,i]
+    end
     
-    g = [_calculate_g(ip, X, h, ue, ξ, d) for d in 1:dim_p]
-    ∇g₃ = [gradient((ξ)->_calculate_g(ip, X, h, ue, ξ, dim_s), ξ)[:, d] for d in 1:dim_p]
-    
-    E = [ _calculate_E(ip, X, ξ, d) for d in 1:dim_p]
-    Dₐ = [gradient((ξ)->_calculate_D(ip, X, h, ξ), ξ)[:,d] for d in 1:dim_p]
+    FI = Tensor{2,dim_p,T,dim_p^2}((α,β)-> E[α]⋅E[β])
+    FII = Tensor{2,dim_p,T,dim_p^2}((α,β)-> E[α] ⋅ (Dₐ[β] * 2/h))
 
-    FI = Tensor{2,dim_p,T}((α,β)-> E[α]⋅E[β])
-    FII = Tensor{2,dim_p,T}((α,β)-> E[α] ⋅ (Dₐ[β] * 2/h))
-
-    #FI = Tensor{2,dim_p,T}((α,β)-> g[α]⋅g[β])
-    #FII = Tensor{2,dim_p,T}((α,β)-> g[α] ⋅ (∇g₃[β] ))
     κ = inv(FI)⋅FII
 
-    λ = [1+κ[i,i]*ξ[dim_s]*h/2 for i in 1:dim_p]
-    #λ = Vec{dim_p,T}(Tuple(λ))
-    
-    return a, da, λ, κ
+    for i in 1:dim_p
+        λ[i] = 1+κ[i,i]*ξ[dim_s]*h/2    
+    end
+    =#
+    return a, da#, λ, κ
 end
